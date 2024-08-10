3822029dc37a9c082c651d4c4db20866676d2f11
R Landau <rflandau@pm.me>
2024-08-10 10:12:24 -0400

    begin mapping tabs display

--- modes/server/server.go
+++ modes/server/server.go
@@ -20,15 +20,29 @@ import (
 	"revolt_tui/broker"
 	"revolt_tui/log"
 	"revolt_tui/modes"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/sentinelb51/revoltgo"
 )
 
+type tab uint8
+
+const (
+	channel tab = iota
+	chat
+)
+
 type Action struct {
-	server  *revoltgo.Server
-	channel *revoltgo.Channel
+	server *revoltgo.Server
+
+	// tab management
+	activeTab tab
+	tabs      []string
+
+	// tab data structs
+	channels tabChannels
 }
 
 var _ modes.Action = &Action{}
@@ -63,6 +77,17 @@ func (a *Action) Update(s *revoltgo.Session, msg tea.Msg) tea.Cmd {
 // Displays the current server, collapsing the channel column automatically if a channel has been
 // selected and the terminal is not wide enough.
 func (a *Action) View() string {
+	var sb strings.Builder
+	sb.WriteString(a.drawTabs())
+}
+
+// helper function for View.
+// Draws the tabs in their current state.
+func (a *Action) drawTabs() string {
+	// if channels tab does not have a channel selected, do not display the chat tab
+	if a.channels.activeChannel == nil {
+		//TODO
+	}
 }
 
 //#endregion

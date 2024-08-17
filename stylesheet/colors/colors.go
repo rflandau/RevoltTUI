a803d8d3d1c89c4f8afa0a089e3432f08e00a20e
R Landau <rflandau@pm.me>
2024-08-17 11:18:26 -0400

    add message-related colors

--- stylesheet/colors/colors.go
+++ stylesheet/colors/colors.go
@@ -2,4 +2,8 @@ package colors
 
 import "github.com/charmbracelet/lipgloss"
 
-const TabBorderForeground lipgloss.Color = "#fd6671" // revolt red
+const (
+	TabBorderForeground lipgloss.Color = "#fd6671" // revolt red
+	MessageTimestamp    lipgloss.Color = "#88968d"
+	MessageAuthor       lipgloss.Color = "#48afc9"
+)
